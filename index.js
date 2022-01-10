b76d55e0bf9b609fc4ecc9a71945e9fdd9606e2b
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-01-10 14:49:42 -0800

    commit tree 5e4260583f82b7b05223e0bfda8453236f0be6f1

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    let firstLetter = string.slice(0,1).toUpperCase()
+    let restOfString = string.slice(1)
+    return firstLetter + restOfString
+
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]/g,'')
+  }
+  static titleize(string){
+    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let wordArray = string.split(' ')
+    
+    for (let i = 0; i < wordArray.length; i++) {
+      if (i === 0) {
+        wordArray[i] = wordArray[i].charAt(0).toUpperCase() + wordArray[i].slice(1)
+      }
+      else if (!except.includes(wordArray[i])) {
+        wordArray[i] = wordArray[i].charAt(0).toUpperCase() + wordArray[i].slice(1)
+      }
+
+    }
+
+    return wordArray.join(' ')
+
+  }
 }
